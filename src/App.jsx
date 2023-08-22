7b949b791f52c3c6152186656133c24b6463e6e4
sairam-maurya <rams44258@gmail.com>
2023-08-22 11:33:31 +0530

    applayout design

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>cities</p>} />
+          <Route path="cities" element={<p>cities</p>} />
+          <Route path="countries" element={<p>countries</p>} />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
