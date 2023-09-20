1cb5b8dee02774050be91df0d973116e40916cc6
sairam-maurya <rams44258@gmail.com>
2023-09-20 23:57:57 +0530

    ..

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useReducer,
 } from "react";
 
-const BASE_URL = "https://world-wise-api-server.vercel.app/";
+const BASE_URL = "https://world-wise-api-server.vercel.app/cities";
 
 const CitiesContext = createContext();
 
